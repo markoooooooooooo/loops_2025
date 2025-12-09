3e10e0ec8a097388cc2a1b7c768440af653a7977
markoooooooooooo <mimunoz4@cps.edu>
2025-12-09 20:42:51 +0000

    loops

--- 0_prelesson_on_loops.py
+++ 0_prelesson_on_loops.py
@@ -1,2 +1,16 @@
-Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
-While loops  
+# Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
+# While loops  
+
+# for loops = execute a block of code a fixed number of times.
+# You can iterate over a range, string, sequence, etc.
+
+# credit_card = "1234-5678-9012-3456"
+
+# for x in credit_card:
+#     print(x)
+
+for x in range (1, 21):
+    if x==13:
+        break
+    else:
+        print(x)
--- 1_loops.py
+++ 1_loops.py
@@ -1,7 +1,7 @@
 # Example Practice:
 # Given this list of fruits:
 fruits = ["apple", "banana", "cherry", "date"]
-
+print(len(fruits))
 # Challenge:
 # Use a for loop to print each fruit on a new line.
 print(fruits[0])
@@ -13,11 +13,25 @@ for fruit in fruits:
 
 # Given a list of school subjects:
 subjects = ["Math", "Science", "History", "Art"]
+print(len(subjects))
 for subject in subjects:
     print(subject)
+
+
 # Challenge:
 # Use a for loop and range to print each subject along with its index:
 # Example output: "Subject 0: Math"
+for subject in subjects:
+    if subject == "History"
+    break
+print(subject)
+for subject in subjects:
+    if subject == "Science":
+        continue
+    print(subject)
+    for index in range(len(subject)):
+        print("Subject" + str(index) + ";" subjects[index])
+
 
 
 # Given:
@@ -25,3 +39,9 @@ numbers = [5, 10, 15, 20]
 
 # Challenge:
 # Use a for loop to add all the numbers and print the total.
+total = 0
+for number in numbers:
+    total += number
+print("total" , total)
+
+new_numbers = list(range(1, 600001))
